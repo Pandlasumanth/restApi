1f6d7bfdbaa9399e808835189619130c0b2829fe
PandlaSumanth <44796404+Pandlasumanth@users.noreply.github.com>
2019-02-08 17:21:19 +0530

    day2

--- test07_02_2019/test07_02_2019/urls.py
+++ test07_02_2019/test07_02_2019/urls.py
@@ -17,7 +17,10 @@ from django.contrib import admin
 from django.urls import path
 from django.views.generic import TemplateView
 
+from testapp import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',TemplateView.as_view(template_name='index.html'))
+    path('',views.ShowIndex),
+    path('newregister/',views.ShowRegister),
 ]
--- test07_02_2019/testapp/views.py
+++ test07_02_2019/testapp/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+def ShowIndex(request):
+    return render(request,"index.html")
+def ShowRegister(request):
+    return render(request,"newregister.html")
